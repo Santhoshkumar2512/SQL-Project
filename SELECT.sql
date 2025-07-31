06b26908781efa6a9c82273f1158b9507d6ca1dc
Santhosh Kumar S <santhoshkumar.s@quadrasystems.net>
2025-07-31 15:47:06 +0530

    Automated commit of changes

--- SELECT.sql
+++ SELECT.sql
@@ -4,3 +4,25 @@ SELECT * FROM Students;
 SELECT * FROM Courses;
 SELECT * FROM Instructors;
 SELECT * FROM Enrollments;
+
+
+SELECT
+    StudentID,
+    FirstName,
+    LastName,
+    EnrollmentDate
+FROM
+    Students
+WHERE
+    EnrollmentDate > '2023-12-31';
+GO
+
+SELECT
+    CourseID,
+    CourseName,
+    CreditHours
+FROM
+    Courses
+WHERE
+    CreditHours = 4;
+GO
